364f42301be11d68144f762fbc3f050203cfb766
Yami Thalyssa <tam@ic.ufal.br>
2019-10-16 22:58:27 -0300

    Update main.java

--- main.java
+++ main.java
@@ -116,6 +116,45 @@ public class main {
         System.out.println("Pedido enviado");
     }
 
+    public static void acceptFriendshipRequest(){
+        Scanner board = new Scanner(System.in);
+        int op = 0;
+        for(int i=0;i<100;i++){
+            if(friendshipRegister[i][userActualID] == 1){
+                System.out.println("Solicitação de amizade de: " + userNames[i] + " deseja aceitar? \n 1 - Sim \n 2 - Não");
+                op = board.nextInt();
+                if(op == 1){
+                    friendshipRegister[userActualID][i] = 1;
+                }else{
+                    friendshipRegister[i][userActualID] = 0;
+                }
+            }
+        }
+    }
+
+    public static void searchProfile(){
+        System.out.println("--PERFIL--");
+        System.out.println("Nome: " + userNames[userActualID]);
+        System.out.println("Login: " + userLogins[userActualID]);
+        System.out.println("\n");
+
+        System.out.println("--LISTA DE AMIGOS--");
+        for(int i=0;i<100;i++){
+            if(friendshipRegister[i][userActualID] == 1 && friendshipRegister[userActualID][i] == 1) {
+                System.out.println(userNames[i]);
+            }
+                System.out.println("\n");
+        }
+
+        System.out.println("\n--COMUNIDADES--");
+        for(int i=0;i<=countCom;i++){
+            if(communitiesOwners[i] == userActualID){
+                System.out.println("Nome: " + communitiesNames[i]);
+            }
+        }
+    }
+
+
     //COMUNIDADES
 
     public static void createCommunity(){
@@ -135,6 +174,38 @@ public class main {
         countCom++;
     }
 
+    public static void joinCommunity(){
+        Scanner keyboard = new Scanner(System.in);
+        int id;
+
+        System.out.println("Digite o ID da comunidade");
+        id = keyboard.nextInt();
+
+        communitiesRegister[id][userActualID] = 1;
+
+        System.out.println("Você agora é um membro desta comunidade!");
+
+    }
+
+    public static void seeCommunities(){
+        for(int i=0;i<countCom;i++){
+            System.out.println(communitiesNames[i]);
+            System.out.println("Dono: " + userNames[communitiesOwners[i]]);
+            System.out.println(communitiesDescriptions[i]);
+            System.out.println("ID de ingresso: " + i + "\n");
+            getMembersList(i);
+        }
+    }
+
+    public static void getMembersList(int communityID){
+        System.out.println("--MEMBROS--");
+        for(int i=0; i<100; i++){
+            if(communitiesRegister[communityID][i] == 1){
+                System.out.println(userNames[i]+"\n");
+            }
+        }
+    }
+
     //GERAL
 
     public static int getUserID(String login){
@@ -146,6 +217,34 @@ public class main {
         return -1;
     }
 
+    public static void signIn(){
+        Scanner board = new Scanner(System.in);
+        String login;
+        String password;
+
+        System.out.println("TELA DE LOGIN");
+        System.out.println("E-mail: ");
+        login = board.nextLine();
+        System.out.println("Senha: ");
+        password = board.nextLine();
+
+        for(int i=0;i<countUsers;i++){
+            if(login.equals(userLogins[i])){
+                if(!password.equals(userPasswords[i])){
+                    System.out.println("Senha incorreta");
+                }else {
+                    //Exibe a tela de login e armazena o ID do usuário para usar nas operações como: Envio de mensagens, edição de perfil, etc.
+                    userActualID = i;
+                    System.out.println("\n");
+                    userLoggedScreen();
+                    return;
+                }
+            }
+        }//Fim do for
+
+        System.out.println("ERRO! Usuário não encontrado\nPor favor, verifique suas informações");
+        exit(0);
+    }
 
     public static void initialScreen(){
         System.out.println("\n\n\n");
@@ -153,6 +252,59 @@ public class main {
         System.out.println("1. Registrar-se");
         System.out.println("2. Login");
         System.out.println("3. Sair");
+    }
+
+    public static void userLoggedScreen(){
+        Scanner keyboard = new Scanner(System.in);
+        int option;
+
+        System.out.println("Bem vindo ao iFace, " + userNames[userActualID] + "!\n Escolha o que deseja fazer: ");
+
+        System.out.println("1 - Editar perfil");
+        System.out.println("2 - Adicionar amigo");
+        System.out.println("3 - Ver pedidos de amizade");
+        System.out.println("4 - Criar comunidade");
+        System.out.println("5 - Ver comunidades");
+        System.out.println("6 - Se juntar a uma comunidade");
+        System.out.println("7 - Verificar informações do perfil");
+        System.out.println("8 - Deletar conta");
+        System.out.println("9 - Sair");
+
+        option = keyboard.nextInt();
+
+        switch (option){
+            case 1:
+                editProfile();
+                break;
+            case 2:
+                sendFriendshipRequest();
+                break;
+            case 3:
+                acceptFriendshipRequest();
+                break;
+            case 4:
+                createCommunity();
+                break;
+            case 5:
+                seeCommunities();
+                break;
+            case 6:
+                joinCommunity();
+                break;
+            case 7:
+                searchProfile();
+                break;
+            case 8:
+                deleteProfile();
+                break;
+            case 9:
+                return;
+            default:
+                System.out.println("Opção inválida!");
+                break;
+
+        }
+
     }
 
     public static void main(String args[]){
